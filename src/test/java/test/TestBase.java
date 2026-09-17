f0a5590f85ba6d7781f1971f6b015a34a92e061e
NerkaKiss <nerkai@gmail.com>
2026-09-17 16:44:35 +0300

    change driver per class

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -7,7 +7,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import utils.AppManager;
 import utils.Driver;
 import utils.PageSourceRecorder;
 import utils.ScreenshotRecorder;
@@ -25,19 +24,12 @@ public abstract class TestBase {
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
         try {
-            // Fresh app launch before every test,
-            // but keep the same Appium/UiAutomator2 session.
-            AppManager.restartApp();
-
-            // First-run UI state only needs to be handled once.
             if (!startupStateHandled) {
                 new CookieBanner().acceptIfDisplayed();
                 new PromoOverlay().dismissIfDisplayed();
                 startupStateHandled = true;
             }
 
-            // Separate recording for every test.
-            // Starts after app restart, so restart itself is not recorded.
             VideoRecorder.startRecording();
 
         } catch (Exception e) {
