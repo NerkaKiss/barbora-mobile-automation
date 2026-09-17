d7ae471cacd99dcaf370fbfb63ce83a971dac394
NerkaKiss <nerkai@gmail.com>
2026-09-17 12:22:25 +0300

    fix overlay logic

--- src/test/java/components/PromoOverlay.java
+++ src/test/java/components/PromoOverlay.java
@@ -2,53 +2,42 @@ package components;
 
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import screens.Common;
 
 import java.time.Duration;
-import java.util.Map;
 
 public class PromoOverlay extends Common {
 
-    private final By promoCloseButton = AppiumBy.xpath(
-            "//android.view.View[@content-desc='Atsisakyti' "
-                    + "and @dismissable='true']"
-                    + "//android.widget.Button[@content-desc='Uždaryti']"
-    );
-
     private static final Duration PROMO_WAIT = Duration.ofSeconds(3);
 
+    private final By promoDismiss =
+            AppiumBy.accessibilityId("Atsisakyti");
+
     public void dismissIfDisplayed() {
+        WebDriverWait promoWait =
+                new WebDriverWait(driver, PROMO_WAIT);
+
         try {
-            WebElement closeButton = new WebDriverWait(driver, PROMO_WAIT)
-                    .until(d -> d.findElements(promoCloseButton)
-                            .stream()
-                            .filter(WebElement::isDisplayed)
-                            .filter(WebElement::isEnabled)
-                            .findFirst()
-                            .orElse(null));
-
-            tap(closeButton);
-
+            promoWait.until(
+                    ExpectedConditions.visibilityOfElementLocated(promoDismiss)
+            );
         } catch (TimeoutException ignored) {
-            // Promo overlay did not appear
+            return;
         }
-    }
 
-    private void tap(WebElement element) {
+        clickOnElement(promoDismiss);
+
         try {
-            element.click();
-        } catch (Exception e) {
-            Rectangle rect = element.getRect();
-            driver.executeScript(
-                    "mobile: clickGesture",
-                    Map.of(
-                            "x", rect.getX() + rect.getWidth() / 2,
-                            "y", rect.getY() + rect.getHeight() / 2
-                    )
+            promoWait.until(
+                    ExpectedConditions.invisibilityOfElementLocated(promoDismiss)
+            );
+        } catch (TimeoutException e) {
+            throw new IllegalStateException(
+                    "Promo overlay was detected but did not close",
+                    e
             );
         }
     }
