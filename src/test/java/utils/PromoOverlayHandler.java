5045c1c21432259f035979deaf8cba97b27ab025
NerkaKiss <nerkai@gmail.com>
2026-09-18 11:59:16 +0300

    change overlay close method

--- src/test/java/utils/PromoOverlayHandler.java
+++ src/test/java/utils/PromoOverlayHandler.java
@@ -4,8 +4,10 @@ import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.util.List;
+import java.util.Map;
 
 public final class PromoOverlayHandler {
 
@@ -31,7 +33,16 @@ public final class PromoOverlayHandler {
             return false;
         }
 
-        closeButtons.getFirst().click();
+        WebElement closeButton = closeButtons.getFirst();
+
+        driver.executeScript(
+                "mobile: clickGesture",
+                Map.of(
+                        "elementId",
+                        ((RemoteWebElement) closeButton).getId()
+                )
+        );
+
         return true;
     }
 }
